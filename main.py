59bc2504925042494140cc6287a8f8e92e751b9a
CamsBams <cs.cameron2000@gmail.com>
2026-02-11 17:05:55 -0600

    Testing commit on VS code to Github.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     #Pygame Setup
     pygame.init()
     maze = Maze("src/maze_layout.json")
-    screen = pygame.display.set_mode((1280,720)) #just made the display standard HD
+    screen = pygame.display.set_mode((1280,720))                    #just made the display standard HD
     pygame.display.set_caption("2D Neuroevolution Maze Runner")
     clock = pygame.time.Clock()
     running = True
@@ -20,11 +20,11 @@ def main():
         maze_background = None
 
     while running:
-        clock.tick(60) #Limit the FPS to 60, we can go lower if we need better performance.
-        #Draw background if its available
+        clock.tick(60)                                                #Limit the FPS to 60, we can go lower if we need better performance.
+        #Draw background if its available.
         if maze_background: 
             screen.blit(maze_background, (0,0))  
-        #Fall back on black background    
+        #Fall back on black background.    
         else:
             screen.fill((0,0,0)) 
         maze.draw(screen)
@@ -47,7 +47,7 @@ def main():
         
     pygame.quit()
 
-
+#Temp edits being made, testing linking github to VSCode.
     
 
 if __name__ == "__main__":
